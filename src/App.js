10e994b589c749b15d27285a641a1915770a148d
Nashbi Grand-Jean <nhgrandjean1@gmail.com>
2022-12-11 16:22:49 -0500

    revolt

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import navBar from "./components/navbar";
+
+
+
+function App() {
+  return (
+    <div>
+      <navBar /> 
+    </div>
+  );
+}
+
+export default App;
